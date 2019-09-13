f81ae2d3bc5acd1023b35b2b95df66d153b3d658
prarabdh <prarabdh@dynapt.ai>
2019-09-13 07:13:43 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Convolutional neural network for image classification',
-    author='opencode@microsoft.com',
+    description='nyctexidata',
+    author='dilipa@dynapt.ai',
     license='MIT',
 )
